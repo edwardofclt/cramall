bbc5a80b7802df29de1acb8af6ff884400d779b1
Eddie Herbert <abeh78@gmail.com>
2026-09-01 17:05:13 -0400

    feat(content): register complete math curriculum

---
+++ src/content/math/index.test.ts
@@ -0,0 +1,13 @@
+import { expect, test } from 'vitest';
+import { PLANNED_LESSONS } from '../curriculum';
+import { lessonsByUnit } from './index';
+
+test('Math registry matches all planned rows in unit order', () => {
+  const expected = PLANNED_LESSONS.filter(({ id }) => id.startsWith('math-'));
+  expect(Object.keys(lessonsByUnit)).toEqual([...new Set(expected.map(({ unitId }) => unitId))]);
+  expect(Object.values(lessonsByUnit).flat().map(({ id, unitId, title, indicatorCodes }) => ({
+    id, unitId, title, indicatorCodes,
+  }))).toEqual(expected.map(({ id, unitId, title, indicatorCodes }) => ({
+    id, unitId, title, indicatorCodes: [...indicatorCodes],
+  })));
+});
--- src/content/math/index.ts
+++ src/content/math/index.ts
@@ -1,6 +1,28 @@
 import type { Lesson } from '../schema';
 import { unit01Lessons } from './u01';
+import { unit02Lessons } from './u02';
+import { unit03Lessons } from './u03';
+import { unit04Lessons } from './u04';
+import { unit05Lessons } from './u05';
+import { unit06Lessons } from './u06';
+import { unit07Lessons } from './u07';
+import { unit08Lessons } from './u08';
+import { unit09Lessons } from './u09';
+import { unit10Lessons } from './u10';
+import { unit11Lessons } from './u11';
+import { unit12Lessons } from './u12';
 
 export const lessonsByUnit: Record<string, Lesson[]> = {
   'math-u01': unit01Lessons,
+  'math-u02': unit02Lessons,
+  'math-u03': unit03Lessons,
+  'math-u04': unit04Lessons,
+  'math-u05': unit05Lessons,
+  'math-u06': unit06Lessons,
+  'math-u07': unit07Lessons,
+  'math-u08': unit08Lessons,
+  'math-u09': unit09Lessons,
+  'math-u10': unit10Lessons,
+  'math-u11': unit11Lessons,
+  'math-u12': unit12Lessons,
 };
